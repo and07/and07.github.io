331b7d364ed61d8d7c995643504fd3366342db57
and07 <and_07@mail.ru>
2016-05-23 15:07:31 +0300

    Update serviceWorker.js

--- js/serviceWorker.js
+++ js/serviceWorker.js
@@ -16,16 +16,17 @@ var config = {
     '/game/index.html',
     '/code/',
     '/code/index.html',
-    '/'
+    '/',
+    '/offline/'
   ],
-  cachePathPattern: /^\/(?:(20[0-9]{2}|about|blog|css|images|js)\/(.+)?)?$/,
+  cachePathPattern: /^\/(?:(20[0-9]{2}|css|images|js)\/(.+)?)?$/,
   offlineImage: '<svg role="img" aria-labelledby="offline-title"'
     + ' viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg">'
     + '<title id="offline-title">Offline</title>'
     + '<g fill="none" fill-rule="evenodd"><path fill="#D8D8D8" d="M0 0h400v300H0z"/>'
     + '<text fill="#9B9B9B" font-family="Times New Roman,Times,serif" font-size="72" font-weight="bold">'
     + '<tspan x="93" y="172">offline</tspan></text></g></svg>',
-  offlinePage: '/'
+  offlinePage: '/offline/'
 };
 
 function cacheName (key, opts) {
