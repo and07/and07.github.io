8fd351d0ac9035e99aef7b5787ededf7bc558137
and07 <and_07@mail.ru>
2016-05-23 12:40:47 +0300

    Update site.js

--- js/site.js
+++ js/site.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./serviceWorker.js', {
-    scope: '/'
+  navigator.serviceWorker.register('/serviceWorker.js', {
+    scope: '/js'
   });
 }
