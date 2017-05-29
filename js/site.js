3acd36c459d2939267cec27d8e54817a89857b31
and07 <and_07@mail.ru>
2017-05-29 23:16:33 +0300

    Update site.js

--- js/site.js
+++ js/site.js
@@ -1,5 +1,18 @@
+/*
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/serviceWorker.js',{scope:'/'});
   //navigator.serviceWorker.register('/serviceWorker.js',{scope:'/code/'});
   //navigator.serviceWorker.register('/serviceWorker.1.js',{scope:'other.html'});
+}
+*/
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/serviceWorker.js').then(function(registration) {
+            // Регистрация успешна
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }).catch(function(err) {
+            // Регистрация не успешна
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    });
 }
