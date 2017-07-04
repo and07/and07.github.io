ca474531804c30e540ba1241271dd9fd9e4665b1
and07 <and_07@mail.ru>
2017-07-04 12:06:27 +0300

    Update and rename blogspot.js to blogspot.html

--- blogspot.js
+++
@@ -1,4 +0,0 @@
-		var gg=document.getElementsByTagName("body")[0],kk=document.createElement("script");
-		kk.type="text/javascript";
-		kk.src="//st-n.ads1-adnow.com/js/ads.js";
-		gg.appendChild(kk);
