a4c2af17600d961a50a7d9fa554d74eba319aed5
and07 <and_07@mail.ru>
2018-04-19 16:18:12 +0300

    Update adduser.sh

--- adduser.sh
+++ adduser.sh
@@ -6,5 +6,9 @@ apt-get clean -y
 apt-get install -f -y
 useradd -m -G sudo -s /bin/bash $1
 passwd $1
-usermod -a -G inet $1
+
 echo '%sudo ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
+
+#for android
+usermod -a -G inet $1
+
