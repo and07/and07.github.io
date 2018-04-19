3394ccb337233f07fa4b29f76c7085a33f8741f9
and07 <and_07@mail.ru>
2018-04-19 14:36:37 +0300

    Update install_golang.sh

--- install_golang.sh
+++ install_golang.sh
@@ -1,11 +1,40 @@
 #!/bin/bash
+
+echo 'Update'
+echo ''
 sudo apt update
+echo ''
+echo ''
+
+echo 'Upgrade'
+echo ''
 sudo apt upgrade
+echo ''
+echo ''
+
+echo 'Figlet'
+echo ''
+sudo apt install figlet
+echo ''
+echo ''
+
+figlet install golang
+
 sudo apt install golang
+
+echo ''
+
+figlet Create Work Dir
+
 mkdir ~/workspace
 mkdir ~/workspace/go
+
+
+figlet Envelopment
+
 export GOPATH=~/workspace/go
 export PATH=$PATH:$GOPATH/bin
 echo "export GOPATH=~/workspace/go" >> ~/.profile
 echo "export PATH=$PATH:$GOPATH/bin" >> ~/.profile
 cat ~/.profile
+
