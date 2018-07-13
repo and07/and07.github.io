9ab1e7b60d10783638482e7471f02dd45f5a03da
and07 <41157447+and07@users.noreply.github.com>
2018-07-13 16:54:02 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,56 +1,6 @@
-//http://getinstance.info/articles/javascript/introduction-to-service-workers/
-var CACHE_NAME = 'my-site-cache-v1';
-var urlsToCache = [
-    '/',
-    '/index.html',
-    '/other.html',
-    '/images/GitHub.png',
-    '/images/2040077.png',
-    '/css/core.css',
-    '/js/site.js'
-];
-
-self.addEventListener('install', function(event) {
-  // установка
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // ресурс есть в кеше
-        if (response) {
-          return response;
-        }
-
-        /* Важно: клонируем запрос. Запрос - это поток, может быть обработан только раз. Если мы хотим использовать объект request несколько раз, его нужно клонировать */
-        var fetchRequest = event.request.clone();
-
-        return fetch(fetchRequest).then(
-          function(response) {
-            // проверяем, что получен корректный ответ
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-            /* ВАЖНО: Клонируем ответ. Объект response также является потоком. */
-            var responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then(function(cache) {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          }
-        );
-      })
-    );
-});
+'use strict';
+importScripts('sw-toolbox.js');
+toolbox.precache(["index.html","style/style.css"]);
+toolbox.router.get("/images/*", toolbox.cacheFirst);
+toolbox.router.get("/*", toolbox.networkFirst, {
+networkTimeoutSeconds: 5});
