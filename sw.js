cea523e884e55e448400c966184a2aa8656fa438
and07 <41157447+and07@users.noreply.github.com>
2018-07-13 18:30:08 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
 importScripts('sw-toolbox.js');
-toolbox.precache(["index.html","/css/core.css"]);
+toolbox.precache(["index.html","css/core.css"]);
 toolbox.router.get("/images/*", toolbox.cacheFirst);
 toolbox.router.get("/icons/*", toolbox.cacheFirst);
 toolbox.router.get("/*", toolbox.networkFirst, {
