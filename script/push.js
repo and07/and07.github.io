f8e89e23ed6f29c00dc042d9f773a1c5a3087a0b
and07 <41157447+and07@users.noreply.github.com>
2019-08-14 13:17:53 +0300

    Update push.js

--- script/push.js
+++ script/push.js
@@ -78,6 +78,24 @@ function unsubscribeUser() {
   });
 }
 
+function askPermission() {
+  return new Promise(function(resolve, reject) {
+    const permissionResult = Notification.requestPermission(function(result) {
+      resolve(result);
+    });
+
+    if (permissionResult) {
+      permissionResult.then(resolve, reject);
+    }
+  })
+  .then(function(permissionResult) {
+    if (permissionResult !== 'granted') {
+      throw new Error('We weren\'t granted permission.');
+    }
+  });
+}
+
+
 function initPush() {
 
   subscribeUser();
