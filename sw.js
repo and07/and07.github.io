8e4e108886410f2d83ff5568d48bc54798c757cf
and07 <41157447+and07@users.noreply.github.com>
2019-08-27 17:23:06 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -108,10 +108,36 @@ self.addEventListener("push", (event) => {
 self.addEventListener('notificationclick', function (event) {
   event.notification.close();
   console.log('self in click event Listener', self)
-  console.log('event', event)
+  console.log('event', event)	
+	
+  if (!event.action) {
+    // Was a normal notification click
+    console.log('Notification Click.');
+    return;
+  }
+
+  switch (event.action) {
+    case 'coffee-action':
+      console.log('User ❤️️\'s coffee.');
+      break;
+    case 'doughnut-action':
+      console.log('User ❤️️\'s doughnuts.');
+      break;
+    case 'gramophone-action':
+      console.log('User ❤️️\'s music.');
+      break;
+    case 'atom-action':
+      console.log('User ❤️️\'s science.');
+      break;
+    default:
+      console.log(`Unknown action clicked: '${event.action}'`);
+      break;
+  }	
+
   event.waitUntil(
     clients.openWindow(event.currentTarget.message_object.link)
   );
+
 });
 
 self.addEventListener('pushsubscriptionchange', function(event) {
