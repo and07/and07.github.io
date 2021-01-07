209fac3d012f83c38d1e02a3ed89653a2a3f06e4
and07 <and_07@mail.ru>
2021-01-07 18:09:41 +0300

    fix

---
+++ js/app.js
@@ -0,0 +1,39 @@
+/*
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/serviceWorker.js',{scope:'/'});
+  //navigator.serviceWorker.register('/serviceWorker.js',{scope:'/code/'});
+  //navigator.serviceWorker.register('/serviceWorker.1.js',{scope:'other.html'});
+}
+*/
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/sw-v2.js').then(function(registration) {
+            // Регистрация успешна
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }).catch(function(err) {
+            // Регистрация не успешна
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+}
+
+/*
+
+     if ('serviceWorker' in navigator && 'PushManager' in window) {
+        function loadScript(src) {
+          return new Promise(function (resolve, reject) {
+            var s;
+            s = document.createElement('script');
+            s.src = src;
+            s.onload = resolve;
+            s.onerror = reject;
+            document.head.appendChild(s);
+          });
+        }
+        loadScript('https://push-notification.news-host.pw/v1/script/push.js');
+	//loadScript('https://push-notifications.prod.news-host.pw/v3/sw-import.js');
+      } else {
+        console.log("Sorry, your browser doesn't support push notifications");
+      }
+      
+      */
